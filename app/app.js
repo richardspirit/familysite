963b5a3068f5a3fc7a21d54de5543ebb4268da06
richardspirit <kennethlync@gmail.com>
2016-10-17 19:43:16 -0600

    Add files via upload

--- app/app.js
+++ app/app.js
@@ -1,85 +1,55 @@
-'use strict';
+angular.module('website', ['ngAnimate'])
+	.controller('MainCtrl', function($scope) {
+        $scope.slides = [
+            {image: 'images/img00.jpg', description: 'Image 00'},
+            {image: 'images/img01.jpg', description: 'Image 01'},
+            {image: 'images/img02.jpg', description: 'Image 02'},
+            {image: 'images/img03.jpg', description: 'Image 03'},
+            {image: 'images/img04.jpg', description: 'Image 04'}
+        ];
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'ngAnimate',
-  'ngTouch',
-  'myApp.home',
-  'myApp.trees',
-  'myApp.version'
-])
-
-.config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
-  $routeProvider.when('/home', {templateUrl: 'templates/home.html'});
-  $routeProvider.when('/trees', {templateUrl: 'templates/trees.html'} );
-  $routeProvider.otherwise({redirectTo: '/templates/home.html'});
-}]);
-
-.controller('MainCtrl', function($scope) {
-	$scope.slides = [
-		{image: 'images/img00.jpg', description: 'Image 00'},
-		{image: 'images/img01.jpg', description: 'Image 01'},
-		{image: 'images/img02.jpg', description: 'Image 02'},
-		{image: 'images/img03.jpg', description: 'Image 03'},
-		{image: 'images/img04.jpg', description: 'Image 04'}
-	];
-	
-	$scope.direction = 'left';
-	$scope.currentIndex = 0;
-	
-	$scope.setCurrentSlideIndex = function(index) {
-		$scope.direction = (index > $scope.currentIndex) ? 'left' : 'right';
-		$scope.currentIndex = index;
-	};
-	
-	$scope.isCurrentSlideIndex = function(index) {
-		return $scope.currentIndex === index;
-	};
-	
-	$scope.prevSlide = function() {
 		$scope.direction = 'left';
-		$scope.currentIndex = ($scope.currentIndex < $scope.slides.length -1) ? ++$scope.currentIndex : 0;
-	};
-	
-	$scope.nextSlide = function() {
-		$scope.direction = 'right';
-		$scope.currentIndex = ($scope.currentIndex > 0) ? --$scope.currentIndex : $scope.slides.length - 1;
-	};
-})
-.animation('.slide-animation', function() {
-	return {
-		beforeAddClass: function(element, className, done) {
-			var scope = element.scope();
-			
-			if (className == 'ng-hide') {
-				var finishPoint = element.parent().width();
-				if(scope.direction !== 'right') {
-					finishPoint = -finishPoint;
-				}
-				TweenMax.to(element, 0.5, {left: finishPoint, onComplete: done });
-			}
-			else {
-				done();
-			}
-		},
-		removeClass: function(element, className, done) {
-			var scope = element.scope();
+		$scope.currentIndex = 0;
 		
-			if (className == 'ng-hide') {
-				element.removeClass('ng-hide');
-				
-				var startPoint = element.parent().width();
-				if(scope.direction === 'right') {
-					startPoint = -startPoint;
+		$scope.setCurrentSlideIndex = function(index) {
+			$scope.direction = (index > $scope.currentIndex) ? 'left' : 'right';
+			$scope.currentIndex = index;
+		};
+		
+		$scope.isCurrentSlideIndex = function(index) {
+			return $scope.currentIndex === index;
+		};
+		
+		$scope.prevSlide = function() {
+			$scope.direction = 'left';
+			$scope.currentIndex = ($scope.currentIndex > 0) ? --$scope.currentIndex : $scope.slides.length - 1;
+		};
+		
+		$scope.nextSlide = function() {
+			$scope.direction = 'right';
+			$scope.currentIndex = ($scope.currentIndex < $scope.slides.length - 1) ? ++$scope.currentIndex : 0;
+		};
+	})
+	.animation('.slide-animation', function() {
+		return {
+			addClass: function (element, className, done) {
+				if (className == 'ng-hide') {
+					TweenMax.to(element, 0.5, {left: -element.parent().width(), onComplete: done });
 				}
-				TweenMax.fromTo(element, 0.5, { left: startPoint }, {left: 0, onComplete: done });
+				else {
+					done();
+				}
+			},
+			removeClass: function (element, className, done) {
+				if (className == 'ng-hide') {
+					element.removeClass('ng-hide');
+					
+					TweenMax.set(element, { left: element.parent().width() });
+					TweenMax.to(element, 0.5, {left: 0, onComplete: done});
+				}
+				else {
+					done();
+				}
 			}
-			else {
-				done();
-			}
-		}
-	};
-});	
-	
+		};
+	});
